04e181d5dc0997e879f9061644ec569d634edf14
shauryapratapsingh5606 <shauryapratapsingh5606@gmail.com>
2026-05-10 15:07:25 +0530

    Update app.py

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ st.write("Upload a crop image to detect possible disease.")
 API_URL = "https://router.huggingface.co/hf-inference/models/linkanjarad/mobilenet_v2_1.0_224-plant-disease-identification"
 
 headers = {
-    "Authorization": "Bearer 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"
+    "Authorization": "Bearer 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"
 }
 
 # IMAGE UPLOAD
